0a53a1ea054a1c1d9c914158816d331828d45032
tcampbell5 <108138470+tcampbell5@users.noreply.github.com>
2023-11-12 00:15:22 -0600

    Add files via upload

--- DNAtoProtein2.py
+++ DNAtoProtein2.py
@@ -21,22 +21,27 @@ class DNAtoProtein2:
     "  661 gaagagtatc gtgtttggtt cgcttcgctc actattacaa agccaaacat attttatctt" +
     "  721 tcttcatagt ttattgtcgt ttcggacaag ttttcttatg taaattgcga cacgaggagt" +
     "  781 cgaaatcgat tttatttcga cgacgagtta gtaaggaagc taggcaaacg ccatagcgat")
-        rna_seq = Seq(transcribe(dna_seq))
-        result_handle = (NCBIWWW.qblast("blastx", "nr", rna_seq,
-                               entrez_query="primate[ORGN]",
-                               format_type="Text"))
-        with open("my_blast.xml", "w") as out_handle:
-            out_handle.write(result_handle.read())
-        result_handle.close()
+        # rna_seq = Seq(transcribe(dna_seq))
+        # result_handle = (NCBIWWW.qblast("blastx", "nr", rna_seq,
+        #                        entrez_query="primates[ORGN]"))
+        # out_handle = open("my_blast.xml", "w")
+        # out_handle.write(result_handle.read())
+        # result_handle.close()
+        # out_handle.close()
         result_handle = open("my_blast.xml")
-        blast_record = NCBIXML.read(result_handle)
-        E_VALUE_THRESH = 0.04
-        for alignment in blast_record.alignments:
-            for hsp in alignment.hsps:
-                if hsp.expect < E_VALUE_THRESH:
-                    title = (alignment.title)
-        return title
-        
+        blast_records = NCBIXML.parse(result_handle)
+        E_VALUE_THRESH = 0.0000000001
+        count = 0
+        titles = []
+        for blast_record in blast_records:
+            for alignment in blast_record.alignments:
+                for hsp in alignment.hsps:
+                    if count < 10 and hsp.expect < E_VALUE_THRESH:
+                        if alignment.title not in titles:
+                            titles.append(alignment.title)
+                            count += 1
+                    else:
+                        break
+        return titles 
 temp = DNAtoProtein2()
 temp.convert("x")
-# print(temp)
