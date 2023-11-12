6931fccdeb7419cf5c496d702dffc00a95b5db30
tcampbell5 <108138470+tcampbell5@users.noreply.github.com>
2023-11-12 05:21:14 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from Bio.Seq import Seq, transcribe
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
+
 NCBIWWW.email = "booey475@gmail.com"
 
 app = Flask(__name__)
@@ -12,26 +13,32 @@ class DNAtoProtein2:
     def convert(dnaString):
         dna_seq = Seq(dnaString)
         rna_seq = Seq(transcribe(dna_seq))
-        result_handle = (NCBIWWW.qblast("blastx", "nr", rna_seq,
-                               entrez_query="primates[ORGN]"))
+        result_handle = (NCBIWWW.qblast("blastx", "nr", rna_seq, entrez_query="primates[ORGN]"))
         out_handle = open("my_blast.xml", "w")
         out_handle.write(result_handle.read())
         result_handle.close()
         out_handle.close()
         result_handle = open("my_blast.xml")
         blast_records = NCBIXML.parse(result_handle)
-        E_VALUE_THRESH = 0.0000000001
-        count = 0
+        e_value_thresh = 0.004
+        common_proteins = ["heat protein", "hemoglobin", "collagen", "actin", "myosin", "keratin", "insulin", 
+                           "albumin", "fibrinogen", "immunoglobulin", "enzyme", "ferritin", "tubulin", "fibronectin", 
+                           "thrombin", "histone", "elastin", "transthyretin", "cytokeratin", "casein", "globulin", 
+                           "myoglobin", "erythropoietin", "prothrombin", "collagenase", "nicotinamide", "lysine", 
+                           "cytochrome"]
+        
         titles = []
+
         for blast_record in blast_records:
             for alignment in blast_record.alignments:
                 for hsp in alignment.hsps:
-                    if count < 3 and hsp.expect < E_VALUE_THRESH:
+                    if hsp.expect < e_value_thresh:
                         if alignment.title not in titles:
-                            titles.append(alignment.title)
-                            count += 1
-                    else:
-                        break
+                            title = alignment.title.split()[1].lower().rstrip(",")
+                            if (title.startswith(("heat", "hsp", "hcg")) or title in common_proteins):
+                                titles.append(title)
+
+        titles = list(set(titles))
         return titles 
 
 temp = DNAtoProtein2()
