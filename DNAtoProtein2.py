b6a94c107f594afba1f8a7f112cddf0ccac19587
tcampbell5 <108138470+tcampbell5@users.noreply.github.com>
2023-11-12 01:35:02 -0600

    Add files via upload

--- DNAtoProtein2.py
+++ DNAtoProtein2.py
@@ -6,28 +6,14 @@ NCBIWWW.email = "booey475@gmail.com"
 
 class DNAtoProtein2:
     def convert(self, dnaString):
-        dnaString = "x"
-        dna_seq = Seq("caaggtcaaa taccaagatt ttttcttctt tgtcagtctt gtccaaacca taagcaagag" +
-    "   61 ctgctgcagt tggttcgtta acaatacgtt ctacttcaag accagcaatt ttaccagcgt" +
-    "  121 cttttgttgc ttgacgttga gcgtcgttga agtaagccgg aactgtgata acagctttgg" +
-    "  181 ttactttctc accaaggtag tcttcagcgt agcctttcaa gtattgaagg atcatagctg" +
-    "  241 agatttcttg tggagtgtat tctttccatt tgcagaaact ttttcagaag ttcccatctt" +
-    "  301 agatttgata gagataactg tatctgggtt tgtaactgct tgacgttttg cagcatcacc" +
-    "  361 acgatgattt ctccgttttt gaatgagact acagatggag ttgtgcggtt tccttctggg" +
-    "  421 tttgcgatga ttttgctttc agttccttca agaactgcaa ctgctgagtt tgttgtacct" +
-    "  481 aagtcaatac cgataatttt agacatgtgt ttttctcctt gttagttaat tttctatttt" +
-    "  541 tctttttgat actcttctta agtggcgaca gtggcgacgc cgtcagagct tgacttcgtc" +
-    "  601 aatctctttg actaaacttt gagcctaagg tctcaaaatt tgcgcaaata gcgccacggc" +
-    "  661 gaagagtatc gtgtttggtt cgcttcgctc actattacaa agccaaacat attttatctt" +
-    "  721 tcttcatagt ttattgtcgt ttcggacaag ttttcttatg taaattgcga cacgaggagt" +
-    "  781 cgaaatcgat tttatttcga cgacgagtta gtaaggaagc taggcaaacg ccatagcgat")
-        # rna_seq = Seq(transcribe(dna_seq))
-        # result_handle = (NCBIWWW.qblast("blastx", "nr", rna_seq,
-        #                        entrez_query="primates[ORGN]"))
-        # out_handle = open("my_blast.xml", "w")
-        # out_handle.write(result_handle.read())
-        # result_handle.close()
-        # out_handle.close()
+        dna_seq = Seq(dnaString)
+        rna_seq = Seq(transcribe(dna_seq))
+        result_handle = (NCBIWWW.qblast("blastx", "nr", rna_seq,
+                               entrez_query="primates[ORGN]"))
+        out_handle = open("my_blast.xml", "w")
+        out_handle.write(result_handle.read())
+        result_handle.close()
+        out_handle.close()
         result_handle = open("my_blast.xml")
         blast_records = NCBIXML.parse(result_handle)
         E_VALUE_THRESH = 0.0000000001
@@ -36,7 +22,7 @@ class DNAtoProtein2:
         for blast_record in blast_records:
             for alignment in blast_record.alignments:
                 for hsp in alignment.hsps:
-                    if count < 10 and hsp.expect < E_VALUE_THRESH:
+                    if count < 3 and hsp.expect < E_VALUE_THRESH:
                         if alignment.title not in titles:
                             titles.append(alignment.title)
                             count += 1
